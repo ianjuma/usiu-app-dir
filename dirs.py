1d045d60bb5271c0ac83ad53b4c909be777aa298
ianjuma <ian.juma@yahoo.com>
2014-02-06 01:01:07 +0300

    dirs only

---
+++ dirs.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+
+# simple python script to get dirs only
+# JIW
+
+import os
+import sys
+
+dir = '.'
+
+software_folders = []
+
+# list comprehension on files
+try:
+    # iterable
+    software_folders += [file for file in os.listdir(dir) if os.path.isfile(file)]
+except IOError as e:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+except OSError as e:
+    print "OS error({0}): {1}".format(e.errno, e.strerror)
+    print "sure the dir is right?"
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
+
+print software_folders
