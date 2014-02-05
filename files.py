72e4694dd85004eb9c67913e383b9c2d73283646
ianjuma <ian.juma@yahoo.com>
2014-02-06 00:57:01 +0300

    files on dir

--- files.py
+++ files.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python
 
 import os
+import sys
+
+dir = '/Data/software'
 
 software_folders = []
 
 # list comprehension on files
 try:
-    software_folders += [for file in os.listdir('/Data/software') if os.path.isfile(file)]
-except IOError:
+    # iterable
+    software_folders += [file for file in os.listdir(dir) if os.path.isfile(file)]
+except IOError as e:
     print "I/O error({0}): {1}".format(e.errno, e.strerror)
+except OSError as e:
+    print "OS error({0}): {1}".format(e.errno, e.strerror)
+    print "sure the dir is right?"
 except:
     print "Unexpected error:", sys.exc_info()[0]
     raise
+
+print software_folders
