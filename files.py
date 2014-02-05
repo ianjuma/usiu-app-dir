0d869c36417b5dba56de6a4709a4382875a836f3
ianjuma <ian.juma@yahoo.com>
2014-02-06 00:52:06 +0300

    file IO exception

--- files.py
+++ files.py
@@ -5,4 +5,10 @@ import os
 software_folders = []
 
 # list comprehension on files
-software_folders += [for file in os.listdir('/Data/software') if os.path.isfile(file)]
+try:
+    software_folders += [for file in os.listdir('/Data/software') if os.path.isfile(file)]
+except IOError:
+    print "I/O error({0}): {1}".format(e.errno, e.strerror)
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
