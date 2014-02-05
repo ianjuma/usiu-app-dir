564e1beab32bae029cb569ddcf33f11bee9c05fa
ianjuma <ian.juma@yahoo.com>
2014-02-05 23:08:14 +0300

    flask dev

--- flask-serve.py
+++ flask-serve.py
@@ -24,8 +24,33 @@ assets.register('js', js)
 
 @app.errorhandler(404)
 def page_not_found(e):
-        # return render_template('modal.html'), 404
-        return "This developer is too lazy to write a 404 :D", 404
+	return make_response(jsonify({"Error 404":
+                                  "Bad request Lazy devloper"}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    return make_response(jsonify({"Error 400":
+                                  "Bad request"}), 400)
+
+
+@app.errorhandler(500)
+def internal_error(e):
+    return make_response(jsonify({"Error 500":
+                                  "Internal Server Error"}), 500)
+
+
+@app.errorhandler(405)
+def invalidMethod(e):
+    return make_response(jsonify({"Error 405":
+                                  "Invalid Request Method"}), 405)
+
+
+@app.errorhandler(410)
+def gone(e):
+    return make_response(jsonify({"Error 410":
+                                  "Resource is Gone"}), 410)
+
 
 
 @app.route('/')
