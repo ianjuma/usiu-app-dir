dc61206fcad81ee0071e49fe6f5e20bb42f801d6
ianjuma <ian.juma@yahoo.com>
2014-02-06 06:35:10 +0300

    modular file

--- files.py
+++ files.py
@@ -10,17 +10,23 @@ dir = '.'
 
 software_files = []
 
-# list comprehension on files
-try:
-    # iterable
-    software_files += [file for file in os.listdir(dir) if os.path.isfile(file)]
-except IOError as e:
-    print "I/O error({0}): {1}".format(e.errno, e.strerror)
-except OSError as e:
-    print "OS error({0}): {1}".format(e.errno, e.strerror)
-    print "sure the dir is right?"
-except:
-    print "Unexpected error:", sys.exc_info()[0]
-    raise
 
-print software_files
+def getFile(software_files):
+    # list comprehension on files
+    try:
+        # iterable
+        software_files += [file for file in os.listdir(
+            dir) if os.path.isfile(file)]
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except OSError as e:
+        print "OS error({0}): {1}".format(e.errno, e.strerror)
+        print "sure the dir is right?"
+    except:
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
+
+if __name__ == "__main__":
+    getFile(software_files)
+    print software_files
